01d5414d500b6366e41b408df423189145e2c3cc
syrrim <syrrim0@gmail.com>
2015-04-16 13:57:18 -0400

    shortcuts don't work in chrome/ie

--- main.js
+++ main.js
@@ -195,6 +195,7 @@ function Controller(backColor, mainColor, errColor, id){
     self = this;
     document.addEventListener("keypress", function(e){
         e = e || window.event;
+        console.log(e)
         if(self.press(e) || self.userin.press(e)){
             e.preventDefault()
         }

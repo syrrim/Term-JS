0268af7dd669c98d211dfa1690a4d0cb98c90861
syrrim <syrrim0@gmail.com>
2015-04-15 21:36:25 -0400

    Made it play nicer with people killing it

--- scripts/head.js
+++ scripts/head.js
@@ -2,7 +2,7 @@ window.process.head = function(args, stdin, stdout, stderr, comm){
     var amount = args[1] !== "" ? args[1] : 10,
         i = 0;
     function run(){
-        if(i<amount){
+        if(i<amount && !comm.dead){
             stdin.readln(function(line){
                 stdout.writeln(line);
                 run()})

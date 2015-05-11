53737c5bd5de0317236d2882e63c8a6872d6b4eb
syrrim <syrrim0@gmail.com>
2015-05-11 19:18:47 -0400

    Update echo.js

--- scripts/echo.js
+++ scripts/echo.js
@@ -1,5 +1,6 @@
 window.process.echo = function(args, stdin, stdout, stderr, communicate){
     message = args.slice(1);
     stdout.writeln(message);
+    console.log(message);
     communicate.finish(0)
 }

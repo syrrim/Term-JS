343b179bf718cf1a1dbb0155c9d2254dbc00b121
syrrim <syrrim0@gmail.com>
2015-06-12 14:27:31 -0400

    Update download.js

--- scripts/download.js
+++ scripts/download.js
@@ -1,6 +1,6 @@
 window.process.download = function(args, io){
     var file = window.dirs.getFile(args[1]),
-        url = URL.createObjectUrl(Blob(file.read())),
+        url = URL.createObjectUrl(new Blob(file.read())),
         link = document.createElement("a");
     link.style.display = "none";
     link.href = url;
